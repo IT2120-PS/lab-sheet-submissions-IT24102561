cc51f7619e29aa5e00d6d412bd911dae7502c73e
IT24102561 <it24102561@my.sliit.lk>
2025-09-15 10:46:36 +0530

    Lab Sheet 06

---
+++ Lab Sheet 06/IT24102561_PS_Lab_06.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\kavin\\Desktop\\IT24102561")
+# Exercise
+# 01)
+# i) Binomial Distribution
+# ii)
+pbinom(46,50,0.85,lower.tail = FALSE)
+
+# 02)
+# i) Number of customer calls per hour
+# ii) Poisson Distribution
+# iii) 
+dpois(15,12)
